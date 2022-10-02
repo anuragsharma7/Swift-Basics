89e444a4a6b8178c6cd6ff167a0cb4d586ba6e73
anuragsharma7 <anurag20c@gmail.com>
2022-10-02 18:00:25 +0530

    String

--- Swift Basics.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ Swift Basics.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -1,10 +1,37 @@
-//: [Previous](@previous)
+
 
 import Foundation
 
 var greeting = "Hello, playground"
 
-//: [Next](@next)
+
+print(greeting)
+
+//Multiline String
+let Anurag = """
+Anurag is a great iOS App developer
+"""
+
+print(Anurag)
+
+
+print("\nGrass is eated by cow \nMy kitten is so furry \tIs this a tab pressed in keyboard? \rCarriage return")
+
+let quote = "\"Imagination is more important than knowledge\" - Einstein"
+print(quote)
+
+let hash = #"Line 1 \nLine 2"# // output will be same as string
+let hash2 = #"Line 1 \#nLine 2"# // putting hash will make it normal
+
+
+let threeMoreDoubleQuotationMarks = #"""
+Here are three more double quotes: """
+"""#
+
+var emptyString = "" //empty string literal
+var anotherEmptyString = String()
+let abc : String = "haha" // String is value type
 
 
 
+ 
