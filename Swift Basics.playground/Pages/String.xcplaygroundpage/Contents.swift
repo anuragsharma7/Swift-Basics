3249c9e00d6808dfa3f42b7975f29a41b45ee712
anuragsharma7 <anurag20c@gmail.com>
2022-10-11 19:29:07 +0530

    String

--- Swift Basics.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ Swift Basics.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
 
 
 import Foundation
+/*
 
 var greeting = "Hello, playground"
 
@@ -30,8 +31,73 @@ Here are three more double quotes: """
 
 var emptyString = "" //empty string literal
 var anotherEmptyString = String()
+ */
+
+/*
 let abc : String = "haha" // String is value type
 
 
+if abc.isEmpty {
+    print("value is there")
+}
 
- 
+var variableString = "Horse"
+variableString += " and carriage" // String mutability
+
+//String are value types
+
+for character in "dog!" {
+    print(character)
+}
+
+// character type annotation
+let exclamationMark : Character = "!"
+
+let catCharacters: [Character] = [ "C","a","t","g","G","A"]
+let catString = String(catCharacters)
+print(catString)
+
+let concatString = catString + variableString
+var myName = "Anurag"
+myName.append(myName)
+print(myName)
+
+//String interpolation
+let message = "\(myName) is my name"
+print(message)
+
+//# will write it as it is
+print(#"Write an interpolated string in Swift using \(multiplier)."#)
+*/
+
+var myName = "Anurag"
+
+//let firstIndx = myName.first
+//let lastIndx = myName.last
+//
+//let eAcute: Character = "\u{E9}"
+//let combinedEAcute: Character = "\u{65}\u{301}" // extended grapheme cluster
+//let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+//// enclosedEAcute is é⃝”
+//
+//
+//let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+
+//print(myName[myName.startIndex])
+//print(myName.endIndex)
+//
+//print(myName[myName.index(before: myName.endIndex)])
+//print(myName[myName.index(after: myName.startIndex)])
+//
+//print(myName.index(myName.startIndex, offsetBy: 6))
+
+//print(myName[myName.endIndex]) //error
+
+for index in myName.indices{
+    print(index)
+}
+
+
+myName.insert("!", at: myName.endIndex)
+print(myName[myName.endIndex])
+
